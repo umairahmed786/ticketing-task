5186ba0f09dfb5a95740cc6d0e4bd723836b7657
umairahmed786 <umair.ahmed@7vals.com>
2024-08-06 14:14:24 +0500

    Install the cancan gem and define abilities  for different roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     if user.role.name == 'owner'
       can :manage, :all
+    elsif user.role.name == 'admin'
+      can :manage, :all 
     elsif user.role.name == 'project_manager'
       can :manage, Project, project_manager: user.id
       can :mange, Issue do|issue|
@@ -12,6 +14,9 @@ class Ability
       can :manage, Comment
     else
       can :manage, Comment
+      can :manage, Issue do |issue|
+        issue.assignee_id == user.id
+      end
     end
   end
 end
