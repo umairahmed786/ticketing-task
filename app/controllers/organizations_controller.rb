f646d87fd21773d4b2425b7ab69c15d8cea44e72
Mehak Nadeem <89253038+MehakNadeem@users.noreply.github.com>
2024-08-07 22:53:19 -0700

    Existing Organization Owner Login Implemented

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,8 +23,33 @@ class OrganizationsController < ApplicationController
       ).to_s
       redirect_to url_with_subdomain
     else
+      flash[:alert] = @organization.errors.full_messages.join(', ')
       render :new
-      flash[:alert] = t('organization.create.failure_message')
+    end
+  end
+
+  def login
+    # This action renders the form where the user inputs their organization site
+  end
+
+  def process_login
+    @organization = Organization.find_by(subdomain: params[:subdomain])
+    owner_role_id = Role.find_by(name: 'owner').id
+    if @organization.present?
+      subdomain = @organization.subdomain
+      host_with_subdomain = "#{subdomain}.#{APP_HOST}"
+      port = 3000
+      # Manually construct the URL with the subdomain using URI module
+      url_with_subdomain = URI::HTTP.build(
+        host: host_with_subdomain,
+        port:,
+        path: new_user_session_path, # Redirect to the login page
+        query: { role_id: owner_role_id }.to_query
+      ).to_s
+      redirect_to url_with_subdomain
+    else
+      flash[:alert] = t('organization.not_found')
+      render :login
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root 'organizations#index'
-  resources :organizations, only: %i[new create]
+  resources :organizations, only: %i[new create index] do 
+    collection do
+      get 'login'
+      post 'process_login'
+    end
+  end
 
   constraints subdomain: /.*/ do
     devise_for :users, controllers: {
+      sessions: 'users/sessions',
       registrations: 'users/registrations',
-      sessions: 'users/sessions'
+      confirmations: 'users/confirmations'
     }
+    resources :owners, only: %i[index]
   end
 end
