c9f79f430230f47b373d991556a7534eaa781de0
Umair Ahmed <umair.ahmed@7vals.com>
2024-08-08 12:25:16 +0500

    Pull the latest development.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations',
-      confirmations: 'users/confirmations'
     }
     resources :projects do
       member do
