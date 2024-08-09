8b2bd082b661f4eacccee4a8fa0729e52709cabd
Mehak Nadeem <89253038+MehakNadeem@users.noreply.github.com>
2024-08-09 11:13:49 -0700

    PR comments resolved; en.yml updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   root 'organizations#index'
   resources :organizations, only: %i[new create index] do 
     collection do
-      get 'login'
-      post 'process_login'
+      get 'render_login_form'
+      post 'login_existing'
     end
   end
 
   constraints subdomain: /.*/ do
     devise_for :users, controllers: {
       sessions: 'users/sessions',
-      registrations: 'users/registrations',
-      confirmations: 'users/confirmations'
+      registrations: 'users/registrations'
     }
     resources :owners, only: %i[index]
   end
